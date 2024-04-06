8205c107bdbb13f9a99d72539f04e3cd0485bad7
Harsh00M <harsh10mahobia@gmail.com>
2024-04-06 17:30:42 +0530

    initial commit

---
+++ app/test/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import  AuroraBackgroundDemo from '@/components/ui/main'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <>
+      <AuroraBackgroundDemo/>
+    </>
+  )
+}
